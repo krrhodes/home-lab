03c416472433fe2d416344c720562c01fc0e04c4
krrhodes <kenai.rhodes@gmail.com>
2025-01-01 11:00:08 -0600

    Fix /etc/hosts cleanup sed command in teardown.sh

--- scripts/teardown.sh
+++ scripts/teardown.sh
@@ -2,4 +2,4 @@
 
 k3d cluster delete $CLUSTER_NAME
 
-sudo sed -i '' '/^### Home Lab Configuration ###$,^### End Lab Configuration ###$/d' /etc/hosts
+sudo sed -i '' '/^### Home Lab Configuration ###$/,/^### End Lab Configuration ###$/{/.*/d;}' /etc/hosts
