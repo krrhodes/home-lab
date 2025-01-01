0d62883e82a65985d98ba4264e0dbc7bd97a795e
krrhodes <kenai.rhodes@gmail.com>
2025-01-01 13:33:05 -0600

    simple crud app

--- simple-crud-app-react-nodejs/frontend/src/services/api.js
+++ simple-crud-app-react-nodejs/frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-	baseURL: 'http://simple-crud-app-react-nodejs-backend:3333/',
+	baseURL: process.env.REACT_APP_API_URL ?? 'http://localhost:3333/',
     headers: { 
         "Content-Type": "application/x-www-form-urlencoded"
     }
